126bdc84edd5ebcb1a8a1e89cd72cf7e869ab391
Shaik Ali <shaikali99086@gmail.com>
2025-06-23 07:31:53 -0700

    More customization

--- app/page.js
+++ app/page.js
@@ -2,6 +2,8 @@ import Navbar from "@/components/navbar";
 import { Manrope } from "next/font/google";
 
 import { IoIosAlert } from "react-icons/io";
+import { GoCheck } from "react-icons/go";
+import { BsArrowRightCircleFill } from "react-icons/bs";
 
 const manrope = Manrope();
 
@@ -9,12 +11,14 @@ export default function Home() {
   return (
     <>
       <section className="h-screen w-screen flex items-center flex-col justify-center">
-        <section className="flex h-130 w-[85vw] md:w-200 bg-neutral-900 rounded-2xl border-1 border-neutral-800 flex-col">
+        <section className="flex w-[85vw] md:w-180 bg-neutral-900 rounded-2xl border-1 border-neutral-800 flex-col">
           <header className="p-10 md:leading-8 leading-7">
             <h2 className={` text-2xl ${manrope.className} mb-3`}>
               They left clues.
             </h2>
-            <p className={`text-xm text-neutral-400 leading-6`}>
+            <p
+              className={`text-[14px] md:text-base text-neutral-400 leading-6`}
+            >
               Studying What Was Never Meant to Be Found
             </p>
           </header>
@@ -22,19 +26,25 @@ export default function Home() {
             <input
               type="text"
               placeholder="Email address"
-              className={`w-full p-5 py-3 outline-none border-b-1 border-neutral-700 mb-10 focus:border-[#006fff]`}
+              className={`w-full p-5 py-3 outline-none border-b-1 border-neutral-700 mb-10 focus:border-sky-600`}
             />
             <input
               type="text"
               placeholder="New password"
-              className="w-full p-5 py-3 outline-none border-b-1 border-neutral-700 focus:border-[#006fff]"
+              className="w-full p-5 py-3 outline-none border-b-1 border-neutral-700 focus:border-sky-600"
             />
-            <p className="p-5 text-red-500 px-0 flex items-center gap-2">
+            {/* <p className="p-5 pb-0 text-red-500 px-0 flex items-center gap-2">
               <IoIosAlert /> Invalid email address, try other.
-            </p>
-            <button className="p-5 bg-sky-600 py-3 rounded-xl float-right mt-6">
-              Enter
-            </button>
+            </p> */}
+            <section className="md:flex flex-row-reverse items-center justify-between mt-5">
+              <button className="p-5 bg-sky-600 py-3 rounded-xl md:float-right flex items-center gap-2 cursor-pointer">
+                Enter <BsArrowRightCircleFill />
+              </button>
+              <p className="p-5 px-0 text-base text-neutral-400">
+                Already have an account?{" "}
+                <span className="underline text-neutral-200">Sign in</span>
+              </p>
+            </section>
           </section>
         </section>
       </section>
--- components/navbar.jsx
+++ components/navbar.jsx
@@ -1,17 +1,22 @@
+import { FaInstagram } from "react-icons/fa";
+import { FaGithub } from "react-icons/fa";
+import { BsTwitterX } from "react-icons/bs";
+import { FaLinkedin } from "react-icons/fa";
+
 export default function Navbar() {
   return (
     <nav className="w-70 bg-neutral-900 border-1 border-neutral-700 h-10 rounded-3xl fixed bottom-10 left-[50%] -translate-x-[50%] grid grid-cols-4 overflow-hidden">
-      <h2 className="text-base cursor-pointer border-r-1 border-neutral-700 hover:bg-[#006fff] flex items-center justify-center">
-        1
+      <h2 className="text-base cursor-pointer border-r-1 border-neutral-700 hover:bg-sky-600 flex items-center justify-center">
+        <FaInstagram />
       </h2>
-      <h2 className="text-base cursor-pointer border-r-1 border-neutral-700 hover:bg-[#006fff] flex items-center justify-center">
-        2
+      <h2 className="text-base cursor-pointer border-r-1 border-neutral-700 hover:bg-sky-600 flex items-center justify-center">
+        <FaGithub />
       </h2>
-      <h2 className="text-base cursor-pointer border-r-1 border-neutral-700 hover:bg-[#006fff] flex items-center justify-center">
-        3
+      <h2 className="text-base cursor-pointer border-r-1 border-neutral-700 hover:bg-sky-600 flex items-center justify-center">
+        <BsTwitterX />
       </h2>
-      <h2 className="text-base cursor-pointer hover:bg-[#006fff] flex items-center justify-center">
-        4
+      <h2 className="text-base cursor-pointer hover:bg-sky-600 flex items-center justify-center">
+        <FaLinkedin />
       </h2>
     </nav>
   );
